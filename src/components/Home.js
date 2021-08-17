ddb82b1ccd3e3a2056c1b4ba1a31917e716c4694
mateoroldos <mateoroldos19@gmail.com>
2021-08-17 16:21:13 -0300

    Home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
-  return <h1>This is the home page</h1>;
+  return (
+    <div className="home-hero">
+      <div>
+        <h2>Welcome To The Football Legends Store</h2>
+        <p>
+          Making your dream team come true was never so easy!
+          <br /> Buy your favourite players and recive them in less than a week.
+        </p>
+        <Link to="/store">
+          <button>Shop Now</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
